0f7c55376d89e5ba422cd78fdfe74e4be610f6ab
Abdullah NUROĞLU <abdullah.nuroglu>
2022-09-19 12:17:28 +0300

    images added

--- W1/w1homework1/src/Template/Footer.js
+++ W1/w1homework1/src/Template/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <>
-            <footer class="w3-center w3-black w3-padding-16">
+            <footer className="w3-center w3-black w3-padding-16">
                 <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" className="w3-hover-text-green">w3.css</a></p>
             </footer>
         </>
